0cce6842f39fde2245b1ab872a6d6a0b1cc067d9
emangor_turner <erik.mangor@turner.com>
2018-01-30 21:27:04 -0500

    update node version and packages, add cpu cluster, remove router file

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const config = require( './config' );
 const express = require('express');
-module.exports.express = express;
-const app = express();
-module.exports.app = app;
-const routes = require('./routes');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const logger = require('./utils/logger');
-app.listen(config.port);
-logger.info(`${config.serviceName} is listening on port: ${config.port}`);
+const bodyParser = require('body-parser');
+//import controllers
+const healthcheckController = require('./controllers/controller-healthcheck');
+const sampleController = require('./controllers/controller-sample');
+
+if (cluster.isMaster) {
+    // create a worker for each CPU
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('online', (worker) => {
+        logger.info(`worker online, worker id: ${worker.id}`);
+    });
+    //if worker dies, create another one
+    cluster.on('exit', (worker, code, signal) => {
+        logger.error(`worker died, worker id: ${worker.id} | signal: ${signal} | code: ${code}`);
+        cluster.fork();
+    });
+} else {
+    //create express app
+    const app = express();
+    const router = express.Router();
+
+    app.use(bodyParser.json());
+    app.use(router);  // tell the app this is the router we are using
+    //healthcheck routes
+    router.get('/', healthcheckController.healthcheck);
+    router.get('/healthcheck', healthcheckController.healthcheck);
+    // sampleController routes
+    router.get('/servertime', sampleController.getTime);
+    router.get('/transaction', sampleController.sampleTransaction);
+
+    app.listen(config.port, function () {
+        logger.info(`worker started: ${cluster.worker.id} | server listening on port: ${config.port}`);
+    });
+}
